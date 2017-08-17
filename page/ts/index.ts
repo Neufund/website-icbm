4c6db13ed91fb1f8985513f041cc9e203d1ca486
mostafabalata <mostafa.balata@gmail.com>
2017-08-17 17:32:04 +0200

    fix reivews

--- page/ts/index.ts
+++ page/ts/index.ts
@@ -10,28 +10,27 @@ vex.registerPlugin(vexDialog)
 
 const getPersonModal: any = function(name: string, image: string, preTitle:string, title: string, bio:string){
     return {
-        input: [
-            '<a href="#" class="close-modal"></a>',
-            '<div class="row">',
-                '<div class="col-md-3 person-info-container">',
-                    '<div class="person-info">',
-                        `<img class="rounded-image" src="${image}"/>`,
-                    '</div>',
-                '</div>',
-                '<div class="col-md-6">',
-                    '<div class="person-details">',
-                        `<h4 class="name">${name}</h4>`,
-                        `<div class="title-container">`,
-                            `<span>${preTitle}</span>`,
-                            `<h4 class="title">${title}</h4>`,
-                        `</div>`,
-                        `<p class="bio">${bio}</p>`,
-                        `<a class="link" href="#">domainname.com</a>`,
-                        `<p class="handle">@testingdata</p>`,
-                    '</div>',
-                '</div>',
-            '</div>'  
-        ].join('')
+        input:
+            `<a href="#" class="close-modal"></a>
+                <div class="row">
+                <div class="col-md-3 person-info-container">
+                    <div class="person-info">
+                        <img class="rounded-image" src="${image}"/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="person-details">
+                        <h4 class="name">${name}</h4>
+                        <div class="title-container">
+                            <span>${preTitle}</span>
+                            <h4 class="title">${title}</h4>
+                        </div>
+                        <p class="bio">${bio}</p>
+                        <a class="link" href="#">domainname.com</a>
+                        <p class="handle">@testingdata</p>
+                    </div>
+                </div>
+            </div>`
     }    
 }
 
@@ -48,8 +47,7 @@ $(document).ready(function(){
 
     const seeMore: string = "+ More";
     const seeLess: string = "- Less"; 
-    $('[id^=team_member_], [id^=advisor_]').click(function(){        
-        console.log("Hey")
+    $('.block-person').click(function(){        
         const name: string = $(this).find('h4.name a').text();
         const image: string = $(this).find('img').attr('src');    
         const title: string = $(this).find('h4.title').text();;

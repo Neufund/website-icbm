6cff7b8d0b9c1231ecb9e548e2b65f1981596e84
Krzysztof Kaczor <chris@kaczor.io>
2017-08-20 13:02:13 +0200

    make navbar sticky, align paragraphs to the left side, some responsiveness related work

--- app/components/Incentive.tsx
+++ app/components/Incentive.tsx
@@ -6,24 +6,22 @@ import { HexagonsStack } from "./HexagonsStack";
 import * as styles from "./Incentive.scss";
 
 export const Incentive: React.SFC = () =>
-  <Grid>
-    <Row>
-      <Col sm={6} className={styles.incentive}>
-        <h1>Community owned investment ecosystem</h1>
+  <Row>
+    <Col sm={6} className={styles.incentive}>
+      <h1>Community owned investment ecosystem</h1>
 
-        <p>
-          Commit funds to invest in the future. As a reward for early participation you will get a
-          token (neumark) representing the platform’s ownership.
-        </p>
-        <p>For more information on the commitment process and its legal aspects read the FAQ</p>
+      <p>
+        Commit funds to invest in the future. As a reward for early participation you will get a
+        token (neumark) representing the platform’s ownership.
+      </p>
+      <p>For more information on the commitment process and its legal aspects read the FAQ</p>
 
-        <Button bsStyle="primary">How to participate</Button>
-      </Col>
-      <Col sm={6}>
-        <HexagonsStack className={styles.hexagons}>
-          <p>Commitment Opportunity starts in:</p>
-          <h1>Future</h1>
-        </HexagonsStack>
-      </Col>
-    </Row>
-  </Grid>;
+      <Button bsStyle="primary">How to participate</Button>
+    </Col>
+    <Col sm={6}>
+      <HexagonsStack className={styles.hexagons}>
+        <p>Commitment Opportunity starts in:</p>
+        <h1>Future</h1>
+      </HexagonsStack>
+    </Col>
+  </Row>;

9f190d0670836fd1136bb85573e31ded93c770ee
mostafabalata <mostafa.balata@gmail.com>
2017-08-22 14:44:11 +0200

    Add a placeholder to the platform and the whitepaper

--- page/ts/index.ts
+++ page/ts/index.ts
@@ -9,10 +9,10 @@ import "!style-loader!css-loader!vex-js/dist/css/vex-theme-os.css";
 vex.defaultOptions.className = 'vex-theme-os'
 vex.registerPlugin(vexDialog)
 
-const getPersonModal: any = function(name: string, image: string, preTitle:string, title: string, bio:string){
+const getPersonModal: any = function (name: string, image: string, preTitle: string, title: string, bio: string) {
     return {
         input:
-            `<a href="#" class="close-modal"></a>
+        `<a href="#" class="close-modal"></a>
                 <div class="row">
                 <div class="col-md-3 person-info-container">
                     <div class="person-info">
@@ -35,64 +35,75 @@ const getPersonModal: any = function(name: string, image: string, preTitle:strin
     }
 }
 
-const getParticipateModal: any = function(){
+const getParticipateModal: any = function (text: string) {
     return {
         input:
-            `<a href="#" class="close-modal"></a>
+        `<a href="#" class="close-modal"></a>
                 <div class="row">
                     <div class="col-md-12">
-                        <h3> How to Participate Manual is coming soon </h3>
+                        <h3> ${text} </h3>
                     </div> 
                 </div>
             </div>`
     }
 }
-$(document).ready(function(){    
+$(document).ready(function () {
     $('.has-carousel').owlCarousel(
-        {   navigation: true, 
+        {
+            navigation: true,
             items: 1,
             singleItem: true,
             lazyLoad: true,
-            dots: true, 
+            dots: true,
             autoPlay: 3000, //Set AutoPlay to 3 seconds
         }
     );
 
     const seeMore: string = "+ More";
-    const seeLess: string = "- Less"; 
-    $('.person-block').click(function(){        
+    const seeLess: string = "- Less";
+    $('.person-block').click(function () {
         const name: string = $(this).find('h4.name a').text();
-        const image: string = $(this).find('img').attr('src');    
+        const image: string = $(this).find('img').attr('src');
         const title: string = $(this).find('h4.position').text();;
-        const bio: string = $(this).find('p.bio').text();    
+        const bio: string = $(this).find('p.bio').text();
         const preTitle: string = $(this).find('span.pre-title').text();
 
         vex.dialog.open(getPersonModal(name, image, preTitle, title, bio))
     });
-    
-    $('body').on('click', '.close-modal' , function(e){
+
+    $('body').on('click', '.close-modal', function (e) {
         e.preventDefault();
         $('.vex.vex-theme-os').trigger('click');
     })
 
-    $('.team .see-more').click(function(){        
+    $('.team .see-more').click(function () {
         $(this).text().trim().toLowerCase() == seeMore.trim().toLowerCase() ? $(this).text(seeLess) : $(this).text(seeMore);
-        $('.team .is-hidden').fadeToggle("slow","linear");        
-    });  
-    
-    $('#how-participate-btn').click(function(){
-        vex.dialog.open(getParticipateModal())
+        $('.team .is-hidden').fadeToggle("slow", "linear");
+    });
+
+    $('#how-participate-btn').click(function () {
+        vex.dialog.open(getParticipateModal('How to Participate Manual is coming soon'))
+    });
+
+    $('#neufund-whitepaper-btn').click(function () {
+        vex.dialog.open(getParticipateModal('Whitepaper is coming soon'))
     })
+
+    $('#platform-btn').click(function () {        
+        vex.dialog.open(getParticipateModal('Platform is coming soon'))
+    })
+
+
 });
 
-$(window).scroll(function(e){
-    const scroll: number = $(window).scrollTop();  
+$(window).scroll(function (e) {
+    const scroll: number = $(window).scrollTop();
     const headerSelector: string = '.navbar.navbar-default.navbar-fixed-top';
-    if(scroll > 20) {
-        if($(headerSelector).hasClass('navbar-no-border'))
+    if (scroll > 20) {
+        if ($(headerSelector).hasClass('navbar-no-border'))
             $(headerSelector).removeClass('navbar-no-border');
     } else {
-        if(!$(headerSelector).hasClass('navbar-no-border'))
+        if (!$(headerSelector).hasClass('navbar-no-border'))
             $(headerSelector).addClass('navbar-no-border');
     }
 });

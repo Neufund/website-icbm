6493b4e84e62b64c0b9ee5e6b816282854f364dd
Moe <mohammed.elsharif@gmail.com>
2017-08-29 12:55:55 +0200

    Simple mocha test

---
+++ test/test.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+describe('Mocha Chai running with typescript', () => {
+  it('should be true', () => {
+    expect(true).to.be.true;
+  });
+});

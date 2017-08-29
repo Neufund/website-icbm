c0dfe0e54f4177924e664874996fb37e4f5c3407
mostafabalata <mostafa.balata@gmail.com>
2017-08-29 14:48:25 +0200

    enhance getEnvValue function condition

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 function getEnvValue(obj: any, key: string) {
-    return obj[key] != undefined ? obj[key] : Error(`${key} is not exists in .env file`)
+    return obj[key] !== undefined ? obj[key] : Error(`${key} is not exists in .env file`);
 }
 
-export const commitmentContractAdress: string = getEnvValue(process.env, 'COMMITMENT_CONTRACT_ADDRESS')
+export const commitmentContractAdress: string = getEnvValue(process.env, 'COMMITMENT_CONTRACT_ADDRESS');

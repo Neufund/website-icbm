6548928a49e9712f8e6503166a06ea5a95d51d81
mostafabalata <mostafa.balata@gmail.com>
2017-08-29 14:36:03 +0200

    add check value for the .env file

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
-const commitmentContractAdress: string = process.env.COMMITMENT_CONTRACT_ADDRESS
-
-export {
-    commitmentContractAdress
+function getEnvValue<T>(obj: any, key: string): T {
+    return obj[key] != undefined ? obj[key] : Error(`${key} is not exists in .env file`)
 }
+
+export const commitmentContractAdress: string = getEnvValue(process.env, 'COMMITMENT_CONTRACT_ADDRESS')

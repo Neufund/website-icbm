a916ed75e794adb9e1a3d2d8f2812d13531faf03
Moe <mohammed.elsharif@gmail.com>
2017-08-29 16:16:36 +0200

    Inegrate Codecov and add tests to travis

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,9 +1,8 @@
 import { expect } from 'chai';
-// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
-// import 'mocha';
 
 describe('Mocha Chai running with typescript', () => {
   it('should be true', () => {
     expect(true).to.be.true;
+    expect(false).to.be.false;
   });
 });

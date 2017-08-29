ec1a931ca88d6c28fb30453512596476aa919a0b
mostafabalata <mostafa.balata@gmail.com>
2017-08-29 15:00:59 +0200

    Fix raising the error

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,8 @@
 function getEnvValue(obj: any, key: string) {
-    return obj[key] !== undefined ? obj[key] : Error(`${key} is not exists in .env file`);
+    if( obj[key] === undefined )
+        throw new Error(`${key} is not exists in .env file`);
+
+    return obj[key];
 }
 
 export const commitmentContractAdress: string = getEnvValue(process.env, 'COMMITMENT_CONTRACT_ADDRESS');

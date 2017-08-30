bf02c4f86b8fe861b28604a253e1607e7814b405
Krzysztof Kaczor <chris@kaczor.io>
2017-08-30 12:44:53 +0200

    fix linting setup

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,9 +1,7 @@
-import { expect } from 'chai';
-// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
-// import 'mocha';
+import { expect } from "chai";
 
-describe('Mocha Chai running with typescript', () => {
-  it('should be true', () => {
+describe("Mocha Chai running with typescript", () => {
+  it("should be true", () => {
     expect(true).to.be.true;
   });
 });

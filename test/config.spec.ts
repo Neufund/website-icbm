8d211bdaa20db97e132e04bd5ba358549d3b0b57
mostafabalata <mostafa.balata@gmail.com>
2017-08-30 17:28:20 +0200

    having test for config.js

---
+++ test/config.spec.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { shallow } from "enzyme";
+import * as React from "react";
+
+describe("Config variables", () => {
+  let savedEnvs: any;
+
+  beforeEach(() => {
+    savedEnvs = process.env;
+    process.env = {};
+  });
+
+  afterEach(() => {
+    process.env = savedEnvs;
+  });
+
+  it("Should load variables", () => {
+    process.env = {
+      COMMITMENT_CONTRACT_ADDRESS: "0x00000000000000000000000000000000000000000",
+    };
+    const { commitmentContractAdress } = require("../app/config");
+    expect(commitmentContractAdress).to.equal(process.env.COMMITMENT_CONTRACT_ADDRESS);
+  });
+
+  it('Should throw error that "Key" is not exists', () => {
+    process.env = {};
+    expect(() => {
+      delete require.cache[require.resolve("../app/config")];
+      const { commitmentContractAdress } = require("../app/config");
+    }).to.throw("COMMITMENT_CONTRACT_ADDRESS is not exists in .env file");
+  });
+});

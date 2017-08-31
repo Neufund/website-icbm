08d26c0f4b51df0b9b42a95f41f0055ce151d0ca
Krzysztof Kaczor <chris@kaczor.io>
2017-08-31 11:42:40 +0200

    bignumber typings + abi fixes

--- app/web3/contracts/index.ts
+++ app/web3/contracts/index.ts
@@ -1,18 +1,19 @@
+import { BigNumber } from "bignumber.js";
 import { promisifyAll } from "bluebird";
 import { web3Instance } from "../web3Provider";
 
 const CommitmentBase = require("./CommitmentBase.json"); // tslint:disable-line
 
-// declare BigNumber = new bignumber.BigNumber;
-
 interface ICrowdsale {
-  startDate: Promise<any>;
-  endDate: Promise<any>;
+  startDateAsync(): Promise<BigNumber>;
+  endDateAsync(): Promise<BigNumber>;
   maxCap: Promise<any>;
   minCap: Promise<any>;
   //  minCapAsync(): Promise<any>;
   //  maxCapAsync(): Promise<any>;
   // commitAsync(params: { value: number; from: string; gas: number }): Promise<{}>;
 }
-export const Crowdsale = (address: string): ICrowdsale =>
-  promisifyAll(web3Instance.eth.contract(CommitmentBase.abi).at(address)) as any;
+export const Crowdsale = (address: string): ICrowdsale => {
+  const ret = promisifyAll(web3Instance.eth.contract(CommitmentBase.abi).at(address)) as any;
+  return ret;
+};
--- app/web3/loadIcoStats.ts
+++ app/web3/loadIcoStats.ts
@@ -4,8 +4,7 @@ import { asEtherNumber, asNumber } from "./utils";
 export async function loadIcoStats(CrowdsaleAddress: string) {
   const lockedAccount = Crowdsale(CrowdsaleAddress);
 
-  // const [startDate] = await Promise.all([lockedAccount.startDate().then(asNumber)]);
-  const [startDate] = await Promise.all([1]);
+  const [startDate] = await Promise.all([lockedAccount.startDateAsync().then(asNumber)]);
 
   return {
     Icostart: startDate,

39f8198575db868b41048368fdffd16bc495e216
mostafabalata <mostafa.balata@gmail.com>
2017-09-06 10:58:00 +0200

    remove unnessery libraries

--- page/ts/faqScroll.js
+++ page/ts/faqScroll.js
@@ -2,72 +2,74 @@
  *
  *  jQuery plugin that handling the smooth 
  *  scrolling for FAQ page
+ * 
+ *  This plugin should be written in type scripting but due to the 
+ *  nature of this plugin it would probably result with :any
+ *  everywhere and thats why it's just JS
+ * 
  */
 
-;(function ($) {
-
-    // Detect the closest number from array of numbers
-    function closest(num, arr) {
-        let curr = arr[0];
-        let diff = Math.abs(num - curr);
-        for (let val = 0; val < arr.length; val++) {
-            let newdiff = Math.abs(num - arr[val]);
-            if (newdiff < diff) {
-                diff = newdiff;
-                curr = arr[val];
-            }
+// Detect the closest number from array of numbers
+function closest(num, arr) {
+    let curr = arr[0];
+    let diff = Math.abs(num - curr);
+    for (let val = 0; val < arr.length; val++) {
+        let newdiff = Math.abs(num - arr[val]);
+        if (newdiff < diff) {
+            diff = newdiff;
+            curr = arr[val];
         }
-        return curr;
     }
+    return curr;
+}
 
 
-    $.fn.faqScroll = function (options) {
+$.fn.faqScroll = function (options) {
 
-        // the options
-        const settings = $.extend({
-            sidebarArea: '#sidebar',
-            offset: 80,
-            speed: 500
-        }, options);
+    // the options
+    const settings = $.extend({
+        sidebarArea: '#sidebar',
+        offset: 80,
+        speed: 500
+    }, options);
 
-        // Handling the click event
-        $(`${settings.sidebarArea} a`).click(function (e) {
-            e.preventDefault();
+    // Handling the click event
+    $(`${settings.sidebarArea} a`).click(function (e) {
+        e.preventDefault();
 
-            $(this).parent().siblings().removeClass('active');
-            $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().addClass('active');
 
-            const dest = $(this).attr("href");
-            if (dest === "#") {
-                return;
-            }
-            const target = $(dest);
-            console.log(target);
-            $("html, body").stop().animate(
-                {
-                    scrollTop: target.offset().top - settings.offset,
-                },
-                settings.speed
-            );
-        });
+        const dest = $(this).attr("href");
+        if (dest === "#") {
+            return;
+        }
+        const target = $(dest);
+        console.log(target);
+        $("html, body").stop().animate(
+            {
+                scrollTop: target.offset().top - settings.offset,
+            },
+            settings.speed
+        );
+    });
 
-        const scrollendDelay = settings.speed; // ms
+    const scrollendDelay = settings.speed; // ms
 
-        // Handling the scroll event
-        $(window).scroll(function (e) {
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function () {
-                let positions = [];
-                let sections = {};                
-                $.each($(`[data-target="${settings.sidebarArea}"]`).children(), (i, section) => {
-                    positions.push($(section).position().top);
-                    sections[$(section).position().top] = `#${$(section).attr('id')}`;
-                });
+    // Handling the scroll event
+    $(window).scroll(function (e) {
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function () {
+            let positions = [];
+            let sections = {};                
+            $.each($(`[data-target="${settings.sidebarArea}"]`).children(), (i, section) => {
+                positions.push($(section).position().top);
+                sections[$(section).position().top] = `#${$(section).attr('id')}`;
+            });
 
-                const activeMenue = closest($(window).scrollTop(), positions);
-                $(`${settings.sidebarArea} > li`).removeClass('active');                
-                $(`a[href="${sections[activeMenue]}"]`).parent('li').addClass('active');
-            }, scrollendDelay/5));
-        });
-    }
-}(jQuery));
+            const activeMenue = closest($(window).scrollTop(), positions);
+            $(`${settings.sidebarArea} > li`).removeClass('active');                
+            $(`a[href="${sections[activeMenue]}"]`).parent('li').addClass('active');
+        }, scrollendDelay/5));
+    });
+}

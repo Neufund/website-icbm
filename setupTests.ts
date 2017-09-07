bbed33d94751b7e9da756af3815752f124bf9b33
Krzysztof Kaczor <chris@kaczor.io>
2017-09-07 16:39:26 +0200

    countdown component tests

--- setupTests.ts
+++ setupTests.ts
@@ -2,6 +2,7 @@ import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as chaiEnzyme from "chai-enzyme";
 import "ignore-styles";
+import "jsdom-global/register";
 import "./app/typings/css-modules"; // this ignores problems with css modules. @todo should load all typings automatically (based on tsconfig.json)
 import "./app/typings/zero-fill";
 
--- test/components/Countdown.spec.tsx
+++ test/components/Countdown.spec.tsx
@@ -1,18 +1,79 @@
 import { expect } from "chai";
-import { shallow } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import * as moment from "moment";
 import * as React from "react";
+import * as sinon from "sinon";
 
 import { Countdown, CountdownComponent } from "../../app/components/Countdown";
 
+const MONTH_IN_SECONDS = 60 * 60 * 24 * 31;
+
 describe("<Countdown />", () => {
-  it("should render", () => {
-    const start = moment("2017-09-15T14:49:32.154");
-    const component = shallow(
-      <Countdown finishDate={start}>
-        <div className="child" />
-      </Countdown>
-    );
-    expect(component).to.contain(CountdownComponent);
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    sandbox.useFakeTimers(); // this seems to not work correctly with moment if exact timestamp is provided
+  });
+
+  afterEach(() => {
+    sandbox.verifyAndRestore();
+  });
+
+  it("should render and tick", () => {
+    const finishDate = moment(MONTH_IN_SECONDS * 2, "X");
+
+    const component = mount(<Countdown finishDate={finishDate} />);
+
+    assertCountdownComponent(component, { days: "62", hours: "00", minutes: "00", seconds: "00" });
+
+    sandbox.clock.tick(2000);
+    component.update();
+
+    assertCountdownComponent(component, { days: "61", hours: "23", minutes: "59", seconds: "58" });
+  });
+
+  it("should render zeroes if it's overdue", () => {
+    const finishDate = moment(-1, "X");
+    const component = mount(<Countdown finishDate={finishDate} />);
+    assertCountdownComponent(component, { days: "00", hours: "00", minutes: "00", seconds: "00" });
+  });
+
+  describe("with callback", () => {
+    it("should fire callback when 0 is reached", () => {
+      const finishDate = moment(2, "X");
+      const onFinishSpy = sandbox.spy();
+
+      const component = mount(<Countdown finishDate={finishDate} onFinish={onFinishSpy} />);
+
+      sandbox.clock.tick(2000);
+      component.update();
+
+      sandbox.clock.tick(2000);
+      component.update();
+
+      expect(onFinishSpy.calledOnce).to.be.true;
+    });
   });
 });
+
+interface ICountdownComponents {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
+function assertCountdownComponent(
+  countdownComponent: ReactWrapper<any, any>,
+  components: ICountdownComponents
+) {
+  const days = countdownComponent.find(`[data-test-id="countdown-days"]`).text();
+  const hours = countdownComponent.find(`[data-test-id="countdown-hours"]`).text();
+  const minutes = countdownComponent.find(`[data-test-id="countdown-minutes"]`).text();
+  const seconds = countdownComponent.find(`[data-test-id="countdown-seconds"]`).text();
+
+  expect(days).to.be.eq(components.days);
+  expect(hours).to.be.eq(components.hours);
+  expect(minutes).to.be.eq(components.minutes);
+  expect(seconds).to.be.eq(components.seconds);
+}

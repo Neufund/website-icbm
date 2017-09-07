36c039fac029e431ff31e766b3be9f182c11ddb7
Krzysztof Kaczor <chris@kaczor.io>
2017-09-07 17:22:33 +0200

    make before ico component refresh when time runs out

--- app/containers/BeforeIco.tsx
+++ app/containers/BeforeIco.tsx
@@ -1,17 +1,19 @@
 import * as moment from "moment";
 import * as React from "react";
-import { connect } from "react-redux";
+import { connect, Dispatch } from "react-redux";
 
+import { loadIcoParams } from "../actions/loadIcoParams";
 import { Incentive } from "../components/Incentive";
+import { IAppState } from "../reducers";
 import { selectStartDate } from "../reducers/icoState";
-import { IAppState } from "../reducers/index";
 
 interface IBeforeIco {
   startDate: moment.Moment;
+  loadIcoParameters: () => {};
 }
-const BeforeIco: React.SFC<IBeforeIco> = ({ startDate }) =>
+const BeforeIco: React.SFC<IBeforeIco> = ({ startDate, loadIcoParameters }) =>
   <div>
-    <Incentive startDate={startDate} />
+    <Incentive startDate={startDate} onFinish={loadIcoParameters} />
   </div>;
 
 export function mapStateToProps(state: IAppState) {
@@ -20,4 +22,10 @@ export function mapStateToProps(state: IAppState) {
   };
 }
 
-export default connect(mapStateToProps)(BeforeIco);
+function mapDispatchToProps(dispatch: Dispatch<IAppState>) {
+  return {
+    loadIcoParameters: () => dispatch(loadIcoParams),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BeforeIco);

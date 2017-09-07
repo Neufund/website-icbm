b84c628a6e69f0e9397ed0a7a9a3e85f3bc421d6
Tomek Banach <tomek@banciur.org>
2017-09-07 15:30:35 +0200

    Initial CommitFundsForm component (not finished)

---
+++ app/muiTheme.tsx
@@ -0,0 +1,7 @@
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+// colors should be same as in common/sass/variables.scss
+export default getMuiTheme({
+  palette: {
+    primary1Color: "#d5e20f",
+  },
+});

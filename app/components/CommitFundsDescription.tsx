cf3175fa9535d6eccaaac68970006d845062839b
Tomek Banach <tomek@banciur.org>
2017-09-10 18:21:17 +0200

    Restyle of CommitFundsDescription

--- app/components/CommitFundsDescription.tsx
+++ app/components/CommitFundsDescription.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+import * as style from "./CommitFundsDescription.scss";
 import { TextCopyable } from "./TextCopyable";
 
 export const CommitFundsDescription: React.SFC = () => (
-  <div>
+  <div className={style.container}>
     <h3>Other options</h3>
     <p>
       <a href="">You can also use MyEtherWallet</a>
@@ -13,11 +14,18 @@ export const CommitFundsDescription: React.SFC = () => (
       </a>
     </p>
     <p>Validate ethereum smart contract address:</p>
-    <TextCopyable text="0x6895304785c271b827f1990860d5093e30d2a121" />
+    <p>
+      <TextCopyable
+        className={style.textCopyable}
+        text="0x6895304785c271b827f1990860d5093e30d2a121"
+      />
+    </p>
     <p>
       <a href="">Verify source code on Etherscan</a>
     </p>
     <p>Include following descriptionin your transaction: </p>
-    <TextCopyable text="0x3c7a3aff" />
+    <p>
+      <TextCopyable className={style.textCopyable} text="0x3c7a3aff" />
+    </p>
   </div>
 );

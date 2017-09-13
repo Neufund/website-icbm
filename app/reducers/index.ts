b9723ab4b2b12bbc38f0e17471c56be7f7ee8f30
Tomek Banach <tomek@banciur.org>
2017-09-13 11:59:17 +0200

    Pass type for form shape in reducers/index.ts

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import { reducer as form } from "redux-form";
+import { FormState, reducer as form } from "redux-form";
 import icoState, { IcoState } from "./icoState";
 
 export interface IAppState {
   readonly icoState: IcoState;
-  readonly form: any;
+  readonly form: FormState;
 }
 
 export default combineReducers<IAppState>({

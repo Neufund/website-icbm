58fbcb0e7c1c950a0ac6b2e18bb97654b3970859
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2017-09-13 18:44:13 +0200

    fixes: 5.6, 5.7, 5.8 (scroll effects)

---
+++ page/js/scroll.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({
+  scale: 1,
+})
+sr.reveal(".text-box")
+sr.reveal(".product-r3-row-wrapper div", 200)
+sr.reveal(".product-r2-row-wrapper div", 200)
+sr.reveal(".img-scroll")

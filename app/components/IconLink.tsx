32673528500f860f2a7caabfd9cf489a664689d0
Tomek Banach <tomek@banciur.org>
2017-09-14 15:24:33 +0200

    IconLink accepts optional className attribute

--- app/components/IconLink.tsx
+++ app/components/IconLink.tsx
@@ -4,10 +4,11 @@ import * as style from "./IconLink.scss";
 interface IIconLink {
   url: string;
   text?: string;
+  className?: string;
 }
 
-export const IconLink: React.SFC<IIconLink> = ({ url, text }) =>
-  <a className={style.link} href={url}>
+export const IconLink: React.SFC<IIconLink> = ({ url, text, className }) =>
+  <a className={className !== undefined ? `${style.link} ${className}` : style.link} href={url}>
     <i className={`material-icons ${style.iconStyle}`}>link</i>
     {text !== undefined ? text : url}
   </a>;

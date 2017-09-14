e4a26476f0a40ecb5037f4e4ebce7a75abd8cbc6
Tomek Banach <tomek@banciur.org>
2017-09-14 16:14:13 +0200

    UnderlinedLink acceptc optional className attribute

--- app/components/UnderlinedLink.tsx
+++ app/components/UnderlinedLink.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
-
 import * as styles from "./UnderlinedLink.scss";
 
 interface IUnderlinedLinkProps {
   href: string;
+  className?: string;
 }
 
-export const UnderlinedLink: React.SFC<IUnderlinedLinkProps> = ({ href, children }) =>
-  <a href={href} className={styles.underlinedLink}>
+export const UnderlinedLink: React.SFC<IUnderlinedLinkProps> = ({ href, className, children }) =>
+  <a
+    href={href}
+    className={
+      className !== undefined ? `${styles.underlinedLink} ${className}` : styles.underlinedLink
+    }
+  >
     {children}
   </a>;

57333bef11fb75e44fc3e9fc9940e64c37baee5f
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2017-09-14 10:37:49 +0200

    working height detection, menu and carousel

--- page/js/scroll.js
+++ page/js/scroll.js
@@ -7,16 +7,47 @@ sr.reveal(".product-r3-row-wrapper div", 200)
 sr.reveal(".product-r2-row-wrapper div", 200)
 sr.reveal(".img-scroll")
 
+
+// carousel
+// siema.min.js
+var carousel = null
+var thr = 768
+function createCarouselIfNecessary() {
+  var w = window.innerWidth
+  console.log(" "+w+" "+thr)
+  if (carousel=== null && w<thr) {
+    carousel = new Siema({
+      selector: '.press .flex-row',
+      perPage: 1,
+      loop: true
+    })
+
+  }
+  if (carousel!==null && w>=thr) {
+    carousel.destroy(true)
+    carousel = null
+  }
+}
+createCarouselIfNecessary()
+
+
+
+
+
 // scroll highlighting
 
 function getScroll() {
   return window.scrollY || document.documentElement.scrollTop
 }
 
-var scrollHeigts = []
-var ul = document.querySelector("#product-menu ul")
+var scrollHeights = []
 window.addEventListener("load", function() {
-  console.log("document load event")
+  // console.log("document load event")
+  var ul = document.querySelector("#product-menu ul")
+  if (!ul) {
+    console.log("no ul")
+    return
+  }
   document.querySelector(".main").childNodes.forEach(function(el){
     var entry = {}
     if (!el.id) return
@@ -34,37 +65,50 @@ window.addEventListener("load", function() {
 
     entry.li = li
 
-    scrollHeigts.push(entry)
+    scrollHeights.push(entry)
 
-    // console.log(entry.id+" "+entry.h+" "+getScroll())
+    console.log(entry.id+" "+entry.h+" "+getScroll())
   })
   handleScroll()
 })
 
 
 function recalculateHeights() {
-  scrollHeigts.forEach(function(entry) {
+  scrollHeights.forEach(function(entry) {
     entry.h = document.getElementById(entry.id).getBoundingClientRect().top + getScroll()
   })
 }
 var currentlyActiveElementIndex = null
+var res = 2 // reserve in pixels
 function handleScroll() {
   var currentHeight = getScroll()
-  for (var i=0; i<scrollHeigts.length-2; i++) {
-    if (currentHeight >= scrollHeigts[i].h && currentHeight < scrollHeigts[i+1].h) {
-      if (currentlyActiveElementIndex===i) return
+  console.log(currentHeight)
+  for (var i=0; i<scrollHeights.length; i++) {
+    if (
+      (i < scrollHeights.length-1 && currentHeight >= (scrollHeights[i].h)-res && currentHeight < (scrollHeights[i+1].h)-res) ||
+      (i == scrollHeights.length-1 && currentHeight >= (scrollHeights[scrollHeights.length-1].h)-res)
+    ) {
+      if (currentlyActiveElementIndex===i) {
+        return
+      }
 
-      // console.log(currentHeight+" "+scrollHeigts[i].h+" "+scrollHeigts[i+1].h)
-
-      var li = scrollHeigts[i].li
+      var li = scrollHeights[i].li
       if (li.className!="product-menu-active") li.className="product-menu-active"
-      if (currentlyActiveElementIndex!==null) scrollHeigts[currentlyActiveElementIndex].li.className=""
+      if (currentlyActiveElementIndex!==null) scrollHeights[currentlyActiveElementIndex].li.className=""
       currentlyActiveElementIndex = i
 
     }
   }
+  console.log(currentlyActiveElementIndex)
 }
-document.addEventListener("resize", recalculateHeights)
 document.addEventListener("scroll", handleScroll)
 
-// "product-menu-active"
+window.addEventListener("resize", function(){
+  createCarouselIfNecessary()
+  recalculateHeights()
+})
+setInterval(function(){
+  if (carousel!==null) {
+    carousel.next(1)
+  }
+}, 2000)

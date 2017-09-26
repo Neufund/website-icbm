60391d7c7990d3cf6232e279eb507c37827f5c08
Tomek Banach <tomek@banciur.org>
2017-09-26 15:59:54 +0200

    Write doc about running parity in dev mode

--- scripts/simulate_traffic.js
+++ scripts/simulate_traffic.js
@@ -1,6 +1,6 @@
 /**
- * script to simulate new blocks on parity in dev mode
- * node scripts/simulate_traffic.js 0x00a329c0648769A73afAc7F9381E08FB43dBEA72 0x009f07E268ca8f4bA9D56Bd2B5dED1fd0D51357C
+ * script to simulate new blocks on parity in dev mode.
+ * See https://github.com/Neufund/commit.neufund.org/blob/master/docs/parity.md#simulating-traffic for details.
  */
 
 const Web3 = require("web3");
@@ -12,4 +12,4 @@ const dst = process.argv[3];
 setInterval( () => {
   web3.eth.sendTransaction({from:src, to:dst, value: web3.toWei(1, "ether")});
   console.log(new Date(), " transferred some eth");
-}, 12000);
+}, 9000);

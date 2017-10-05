887711244a64539e213491dfddfb1d50dc4aa0d1
Tomek Banach <tomek@banciur.org>
2017-10-05 18:39:11 +0200

    Move jQuery to show footer call to action

--- app/actions/loadUserAccount.ts
+++ app/actions/loadUserAccount.ts
@@ -6,7 +6,6 @@ import { loadUserAccountFromWeb3 } from "../web3/loadUserAccountFromWeb3";
 import { SET_USER_ACCOUNT } from "./constants";
 
 export function setUserAccountAction(account: string): IStandardReduxAction {
-  jQuery(".footer").removeClass("hidden"); // this has to be done this ugly way as footer is created outside of react app
   return {
     type: SET_USER_ACCOUNT,
     payload: {
@@ -19,6 +18,7 @@ export const loadUserAccount: ThunkAction<{}, IAppState, {}> = async (dispatcher
   const account = await loadUserAccountFromWeb3();
   const { userState } = getState();
   if (account !== userState.address || userState.loading) {
+    jQuery(".footer").removeClass("hidden"); // this has to be done this ugly way as footer is created outside of react app
     dispatcher(setUserAccountAction(account));
   }
 };

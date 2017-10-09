34e477a1e52083288a84281d174a1bb78b2450a0
Krzysztof Kaczor <chris@kaczor.io>
2017-10-09 18:07:04 +0200

    wording

--- app/components/Incentive.tsx
+++ app/components/Incentive.tsx
@@ -42,19 +42,20 @@ export const Incentive: React.SFC<IIncentive> = ({ startDate, onFinish, neumarkI
       <h1>Community-owned Fundraising Platform</h1>
 
       <p>
-        Neufund bridges blockchain and venture capital enabling ICOs for on- and off-chain startups.
+        Neufund bridges blockchain and venture capital, enabling ICOs for on- and off-chain
+        startups.
       </p>
       <p>
         Reserve funds<sup>
           <a href="#how-it-works">01</a>
         </sup>{" "}
-        today for your future investments. Maintain full control of your investment decisions.
+        today for your future investments. Maintain full control over your investment decisions.
       </p>
       <p>
         Get Neumarks<sup>
           <a href="#why-participate">02</a>
         </sup>{" "}
-        just by reserving funds and become a co-owner of the platform.
+        simply by reserving funds, and become a co-owner of the ecosystem.
       </p>
     </Col>
     <Col sm={6} xsHidden>

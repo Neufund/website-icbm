d92a542ec1f9cb5fdba1cd14045a6c1a7111077c
Krzysztof Kaczor <chris@kaczor.io>
2017-10-23 17:29:22 +0200

    fix startup function to be really async

--- app/startup.ts
+++ app/startup.ts
@@ -9,8 +9,10 @@ import { asyncSessionStorage } from "redux-persist/storages";
 import reducers from "./reducers";
 import { initRepository } from "./web3/contracts/ContractsRepository";
 import { initWeb3 } from "./web3/web3Provider";
+import { promisify } from "bluebird";
 
-// this is pseudo async function as we wait just for one promise and persistStore should be promisified
+const persistStorePromised = promisify(persistStore) as any;
+
 export async function startup(render: (store: Store<any>) => void) {
   const enhancers = () =>
     composeWithDevTools(compose(applyMiddleware(reduxThunk, reduxLogger), autoRehydrate()));
@@ -26,22 +28,6 @@ export async function startup(render: (store: Store<any>) => void) {
     const ReactDebugTool = require("react-dom/lib/ReactDebugTool");
     ReactDebugTool.beginProfiling();
 
-    // Enable Webpack hot module replacement
-    if ((module as any).hot) {
-      // Replace reducers
-      // (module as any).hot.accept('./reducers', () => {
-      //   const newReducer = require('./reducers').default;
-      //   store.replaceReducer(newReducer);
-      // });
-      // Replace routes and components
-      // (module as any).hot.accept('./routes', () => {
-      //   const newAppRoutes = require('./routes').default;
-      //   // NOTE: We don't update the store middleWare,
-      //   //       if you change routes you need to refresh.
-      //   render(store, newAppRoutes);
-      // });
-    }
-
     // we require this files only to track changes in them automatically
 
     // tslint:disable-next-line
@@ -50,12 +36,13 @@ export async function startup(render: (store: Store<any>) => void) {
     require("!raw-loader!../dist/app.css");
   }
 
-  persistStore(
+  await persistStorePromised(
     store,
     {
       whitelist: ["legalAgreementState"],
       storage: asyncSessionStorage,
-    },
-    () => initRepository().then(() => render(store))
+    }
   );
+  await initRepository();
+  render(store)
 }

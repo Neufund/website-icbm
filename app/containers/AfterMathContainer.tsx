37ff74f395416ba5d65a59d3db44108fb34f4575
Tomek Banach <tomek@banciur.org>
2017-11-01 13:24:20 +0100

    AfterMathContainer remove unnecessary jQuery call

--- app/containers/AfterMathContainer.tsx
+++ app/containers/AfterMathContainer.tsx
@@ -16,25 +16,21 @@ interface IAfterMathContainer {
   isKnownUser: boolean;
 }
 
-export const AfterMathContainer: React.SFC<IAfterMathContainer> = ({ isKnownUser }) => {
-  jQuery(".footer").removeClass("hidden"); // this has to be done this ugly way as footer is created outside of react app
-  return (
-    <div>
-      <LegalModal />
-      <CommitNavbar>Commit funds in Neufund Commitment Opportunity</CommitNavbar>
-      <Grid>
-        <Row>
-          <Col xs={12} className={layoutStyle.contentContainer}>
-            <CommitHeaderComponent number="01" title="After math" />
-            {isKnownUser
-              ? <CommitKnownUserAftermathContainer />
-              : <CommitUnknownUserAftermathContainer />}
-          </Col>
-        </Row>
-      </Grid>
-    </div>
-  );
-};
+export const AfterMathContainer: React.SFC<IAfterMathContainer> = ({ isKnownUser }) =>
+  <div>
+    <LegalModal />
+    <CommitNavbar>Commit funds in Neufund Commitment Opportunity</CommitNavbar>
+    <Grid>
+      <Row>
+        <Col xs={12} className={layoutStyle.contentContainer}>
+          <CommitHeaderComponent number="01" title="After math" />
+          {isKnownUser
+            ? <CommitKnownUserAftermathContainer />
+            : <CommitUnknownUserAftermathContainer />}
+        </Col>
+      </Row>
+    </Grid>
+  </div>;
 
 const mapStateToProps = (state: IAppState) => {
   return {

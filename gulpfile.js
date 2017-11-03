d16ed7fcad5fc636e711179b79bd75b1120d5e19
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2017-11-03 10:17:57 +0100

    content rearrangement, fonts, spacing

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,8 @@ gulp.task("page", () => {
     GA_ID: process.env.GA_ID,
     FB_PIXEL_ID: process.env.FB_PIXEL_ID,
     FAQ_ENABLED:!!parseInt(process.env.FAQ_ENABLED),
-    PLATFORM_ENABLED:!!parseInt(process.env.PLATFORM_ENABLED),
+    /*PLATFORM_ENABLED:!!parseInt(process.env.PLATFORM_ENABLED),*/
+    PLATFORM_ENABLED: true
   };
 
   // notice no return here: https://github.com/rogeriopvl/gulp-ejs/issues/86

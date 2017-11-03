be536a5c59c38406826fa3ef7db6538171df3917
Krzysztof Kaczor <chris@kaczor.io>
2017-11-03 11:39:16 +0100

    lint fix

--- app/components/LegalModal.tsx
+++ app/components/LegalModal.tsx
@@ -53,36 +53,36 @@ export const LegalModal: React.SFC<InjectedFormProps & ILegalModalProps> = ({
       <Modal.Footer>
         <div className={styles.checkboxes}>
           <CheckboxField
-              name="reservationAgreement"
-              component={Checkbox}
-              className={styles.checkbox}
-              props={{
-                label: (
-                  <span>
-                    Check here to confirm that you have read, understand and agree to the{" "}
-                    <span className={styles.documentAccent}>Reservation Agreement</span> and{" "}
-                    <span className={styles.documentAccent}>Neumark Token Holder Agreement</span>{" "}
-                    presented above.
-                  </span>
-                ),
-              }}
-              validate={[requiredFieldValidator]}
-            />
-            <CheckboxField
-              name="tokenHolderAgreement"
-              component={Checkbox}
-              className={styles.checkbox}
-              props={{
-                label: (
-                  <span>
-                    Check here to confirm that you are NOT a citizen, resident or entity of the USA or
-                    any other jurisdiction in which it is not permissible to participate in token crowd
-                    contributions or acting on behalf of any of them.
-                  </span>
-                ),
-              }}
-              validate={[requiredFieldValidator]}
-            />
+            name="reservationAgreement"
+            component={Checkbox}
+            className={styles.checkbox}
+            props={{
+              label: (
+                <span>
+                  Check here to confirm that you have read, understand and agree to the{" "}
+                  <span className={styles.documentAccent}>Reservation Agreement</span> and{" "}
+                  <span className={styles.documentAccent}>Neumark Token Holder Agreement</span>{" "}
+                  presented above.
+                </span>
+              ),
+            }}
+            validate={[requiredFieldValidator]}
+          />
+          <CheckboxField
+            name="tokenHolderAgreement"
+            component={Checkbox}
+            className={styles.checkbox}
+            props={{
+              label: (
+                <span>
+                  Check here to confirm that you are NOT a citizen, resident or entity of the USA or
+                  any other jurisdiction in which it is not permissible to participate in token
+                  crowd contributions or acting on behalf of any of them.
+                </span>
+              ),
+            }}
+            validate={[requiredFieldValidator]}
+          />
         </div>
 
         <a href="/" className="btn btn-white">

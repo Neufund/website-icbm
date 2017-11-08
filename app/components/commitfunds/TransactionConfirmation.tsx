410758352f7eab5e559b549cc82985cafc3c0412
Tomek Banach <tomek@banciur.org>
2017-11-08 03:07:03 +0100

    TransactionConfirmation redo
    Show latest block number instead history
    Change design of component

--- app/components/commitfunds/TransactionConfirmation.tsx
+++ app/components/commitfunds/TransactionConfirmation.tsx
@@ -1,3 +1,4 @@
+import { FontIcon } from "material-ui";
 import * as React from "react";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
@@ -10,64 +11,93 @@ import { IAppState } from "../../reducers/index";
 import { ITransactionState } from "../../reducers/transactionState";
 import EtherScanLink from "../EtherScanLink";
 import { LoadingIndicator } from "../LoadingIndicator";
-import SignInstruction from "./SignInstruction";
 import * as styles from "./TransactionConfirmation.scss";
 
-interface IBlockHistoryComponent {
-  blockHistory: Array<{ blockNo: number; confirmedTx: boolean }>;
+const TxSigningComponent: React.SFC<{ handleBackClick: () => void }> = ({ handleBackClick }) =>
+  <div>
+    <p>
+      Please confirm transaction using signer or{" "}
+      <a href="#" onClick={handleBackClick}>
+        back to change your commit
+      </a>.
+    </p>
+    <LoadingIndicator className={styles.loadingIndicator} />
+    <p className={styles.waitingDesc}>Waiting for signing and sending to eth node.</p>
+  </div>;
+
+function mapDispatchToPropsTxSigning(dispatch: Dispatch<any>) {
+  return {
+    handleBackClick: (e: any) => {
+      e.preventDefault();
+      dispatch(transactionResetAction());
+    },
+  };
 }
 
-const BlockHistoryComponent: React.SFC<IBlockHistoryComponent> = ({ blockHistory }) => {
-  return (
-    <table className={styles.blockHistory}>
-      <thead>
-        <tr>
-          <th>block number</th>
-          <th>transaction confirmed</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blockHistory.map(block =>
-          <tr key={block.blockNo}>
-            <td>
-              {block.blockNo}
-            </td>
-            <td>
-              {block.confirmedTx ? "yes" : "no"}
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  );
+const TxSigning = connect(null, mapDispatchToPropsTxSigning)(TxSigningComponent);
+
+const TxMiningComponent: React.SFC<{ txHash: string; blockCurrent: number }> = ({
+  txHash,
+  blockCurrent,
+}) =>
+  <div>
+    <p>
+      You transaction {" "}
+      <EtherScanLink
+        target="_blank"
+        linkType={EtherScanLinkType.TRANSACTION}
+        resourceId={txHash}
+        className={styles.tx}
+      />{" "}
+      is ready.
+    </p>
+    <p>
+      Block number: <b>{blockCurrent}</b>
+      <br />
+      Transaction confirmed: <b>No</b>
+    </p>
+    <LoadingIndicator className={styles.loadingIndicator} />
+    <p className={styles.waitingDesc}>Waiting for confirmation from network</p>
+  </div>;
+
+const iconStyles = {
+  width: "64px",
+  height: "64px",
+  fontSize: "64px",
 };
 
-interface ITransactionSummaryComponent {
+const TxSummaryComponent: React.SFC<{
   txHash: string;
   blockOfConfirmation: number;
   generatedNEU: string;
-}
-
-const TransactionSummaryComponent: React.SFC<ITransactionSummaryComponent> = ({
-  txHash,
-  blockOfConfirmation,
-  generatedNEU,
-}) =>
-  <div className={styles.confirmation} data-test-id="transaction-summary">
-    Your transaction{" "}
-    <EtherScanLink
-      target="_blank"
-      linkType={EtherScanLinkType.TRANSACTION}
-      resourceId={txHash}
-    />{" "}
-    has been mined in block:{" "}
-    <EtherScanLink
-      target="_blank"
-      linkType={EtherScanLinkType.BLOCK}
-      resourceId={blockOfConfirmation}
-    />
-    <br />
-    Neumark generated: {generatedNEU}
+}> = ({ txHash, blockOfConfirmation }) =>
+  <div>
+    <p>
+      You transaction{" "}
+      <EtherScanLink
+        target="_blank"
+        linkType={EtherScanLinkType.TRANSACTION}
+        resourceId={txHash}
+        className={styles.tx}
+      />{" "}
+      is ready.
+    </p>
+    <p>
+      Block number:{" "}
+      <EtherScanLink
+        target="_blank"
+        linkType={EtherScanLinkType.BLOCK}
+        resourceId={blockOfConfirmation}
+      />
+      <br />
+      Transaction confirmed: <b>Yes</b>
+    </p>
+    <div className={styles.confirmedIndicator}>
+      <FontIcon className="material-icons" style={iconStyles}>
+        done
+      </FontIcon>
+    </div>
+    <p className={styles.waitingDesc}>Transaction confirmed</p>
   </div>;
 
 interface ITransactionConfirmationExtras {
@@ -80,56 +110,45 @@ export const TransactionConfirmationComponent: React.SFC<
 > = ({
   txHash,
   blockOfConfirmation,
-  blockHistory,
+  blockCurrent,
   txConfirmed,
   error,
   generatedNEU,
   handleGoToAftermathButton,
   handleTransactionResetButton,
-}) => {
-  const showLoading = generatedNEU === null && error === null;
-  return (
-    <div>
-      <h2>Transaction status</h2>
-      {error !== null &&
-        <div>
-          <div className={styles.error}>
-            {error}
-          </div>
-          <Button bsStyle="primary" onClick={handleTransactionResetButton}>
-            reset transaction
-          </Button>
-        </div>}
-      {showLoading && <LoadingIndicator className={styles.loadingIndicator} />}
-      {txHash === null && <SignInstruction />}
-      {txHash !== null &&
-        !txConfirmed &&
-        <div>
-          <div>
-            You transaction{" "}
-            <EtherScanLink linkType={EtherScanLinkType.TRANSACTION} resourceId={txHash} /> is ready.
-            Now we are waiting for confirmation from network.
-          </div>
-          {blockHistory.length > 0 && <BlockHistoryComponent blockHistory={blockHistory} />}
-        </div>}
-      {txConfirmed &&
-        <TransactionSummaryComponent
-          txHash={txHash}
-          blockOfConfirmation={blockOfConfirmation}
-          generatedNEU={generatedNEU}
-        />}
-      {txConfirmed &&
+}) =>
+  <div>
+    {error !== null &&
+      <div>
+        <div className={styles.error}>
+          {error}
+        </div>
+        <Button bsStyle="primary" onClick={handleTransactionResetButton}>
+          reset transaction
+        </Button>
+      </div>}
+    {txHash === null && <TxSigning />}
+    {txHash !== null &&
+      !txConfirmed &&
+      <TxMiningComponent txHash={txHash} blockCurrent={blockCurrent} />}
+    {txConfirmed &&
+      <TxSummaryComponent
+        txHash={txHash}
+        blockOfConfirmation={blockOfConfirmation}
+        generatedNEU={generatedNEU}
+      />}
+    {txConfirmed &&
+      <div className={styles.goAftermath}>
         <Button bsStyle="primary" onClick={handleGoToAftermathButton}>
           see aftermath page
-        </Button>}
-    </div>
-  );
-};
+        </Button>
+      </div>}
+  </div>;
 
 const mapStateToProps = (state: IAppState) => ({
   txHash: state.transactionState.txHash,
   blockOfConfirmation: state.transactionState.blockOfConfirmation,
-  blockHistory: state.transactionState.blockHistory,
+  blockCurrent: state.transactionState.blockCurrent,
   txConfirmed: state.transactionState.txConfirmed,
   error: state.transactionState.error,
   generatedNEU: state.transactionState.generatedNEU,

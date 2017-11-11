65e83e3eb2d299faf2c5843b3d0bddb575e499ea
Tomek Banach <tomek@banciur.org>
2017-11-11 03:07:20 +0100

    Format gas price and limit

--- app/components/commitfunds/CommitUnknownUserDesc.tsx
+++ app/components/commitfunds/CommitUnknownUserDesc.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Col, Row } from "react-bootstrap";
 
 import { TokenType } from "../../actions/constants";
+import { Web3Service } from "../../web3/web3Service";
 import MoneyComponent from "../MoneyComponent";
 import { TextCopyable } from "../TextCopyable";
 import { UnderlinedLink } from "../UnderlinedLink";
@@ -22,38 +23,55 @@ export const CommitUnknownUserDesc: React.SFC<ICommitUnknownUserDesc> = ({
   gasPrice,
   gasLimit,
   minTicketSize,
-}) =>
-  <div className={style.infoContainer}>
-    <p>
-      Include following data in your transaction:<br />
-      <TextCopyable copyIconOnRight className={style.textCopyable} text={transactionPayload} />
-    </p>
-    <p>
-      Ethereum smart contract address:<br />
-      <TextCopyable copyIconOnRight className={style.textCopyable} text={contractAddress} /> <br />
-      <UnderlinedLink
-        href="https://etherscan.io/address/0xf432cec23b2a0d6062b969467f65669de81f4653"
-        className={style.verifyLink}
-      >
-        Verify source code on Etherscan
-      </UnderlinedLink>
-    </p>
-    <Row className={style.gasSection}>
-      <Col sm={4}>
-        Suggested gas price<br />
-        <TextCopyable copyIconOnRight className={style.textCopyable} text={gasPrice} />
-      </Col>
-      <Col sm={4}>
-        Suggested gas limit<br />
-        <TextCopyable copyIconOnRight className={style.textCopyable} text={gasLimit} />
-      </Col>
-      <Col sm={4}>
-        Minimum ticket size<br />
-        <MoneyComponent
-          containerClass={style.ethAmount}
-          tokenType={TokenType.ETHER}
-          value={minTicketSize}
-        />
-      </Col>
-    </Row>
-  </div>;
+}) => {
+  const gasInGwei = Web3Service.instance.rawWeb3.fromWei(gasPrice, "gwei");
+  const formattedLimit = parseInt(gasLimit, 10).toLocaleString();
+
+  return (
+    <div className={style.infoContainer}>
+      <p>
+        Include following data in your transaction:<br />
+        <TextCopyable copyIconOnRight className={style.textCopyable} text={transactionPayload} />
+      </p>
+      <p>
+        Ethereum smart contract address:<br />
+        <TextCopyable copyIconOnRight className={style.textCopyable} text={contractAddress} />{" "}
+        <br />
+        <UnderlinedLink
+          href="https://etherscan.io/address/0xf432cec23b2a0d6062b969467f65669de81f4653"
+          className={style.verifyLink}
+        >
+          Verify source code on Etherscan
+        </UnderlinedLink>
+      </p>
+      <Row className={style.gasSection}>
+        <Col sm={4}>
+          Suggested gas price<br />
+          <TextCopyable
+            copyIconOnRight
+            className={style.textCopyable}
+            clipText={gasInGwei}
+            text={`${gasInGwei} gwei`}
+          />
+        </Col>
+        <Col sm={4}>
+          Suggested gas limit<br />
+          <TextCopyable
+            copyIconOnRight
+            className={style.textCopyable}
+            clipText={gasLimit}
+            text={formattedLimit}
+          />
+        </Col>
+        <Col sm={4}>
+          Minimum ticket size<br />
+          <MoneyComponent
+            containerClass={style.ethAmount}
+            tokenType={TokenType.ETHER}
+            value={minTicketSize}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};

44c9b002320b236744b0e40b61abe85136bf4a86
Krzysztof Kaczor <chris@kaczor.io>
2017-11-15 14:09:43 +0100

    change url on address input, fix support for "enter" key

--- app/containers/CommitUnknownUserAftermathContainer.tsx
+++ app/containers/CommitUnknownUserAftermathContainer.tsx
@@ -5,6 +5,7 @@ import { compose } from "redux";
 import { Field, FormSubmitHandler, InjectedFormProps, reduxForm } from "redux-form";
 import { TextField } from "redux-form-material-ui";
 
+import { withRouter } from "react-router";
 import { loadAftermathDetails } from "../actions/aftermathActions";
 import { TokenType } from "../actions/constants";
 import { getReservationAgreementTags, getTokenHolderAgreementTags } from "../actions/getTags";
@@ -35,6 +36,7 @@ interface ICommitFundsUnknownUserAftermathProps {
   reservationAgreementHash: string;
   tokenHolderAgreementHash: string;
   neumarkBalance: string;
+  router: any;
 }
 
 interface ICommitFundsUnknownUserAftermathForm {
@@ -51,105 +53,121 @@ const estTextFieldStyles = {
   },
 };
 
-export const CommitUnknownUserAftermath: React.SFC<
-  InjectedFormProps & ICommitFundsUnknownUserAftermathProps
-> = ({
-  lockedAmountEth,
-  unlockDateEth,
-  neumarkBalanceEth,
-  lockedAmountEur,
-  unlockDateEur,
-  neumarkBalanceEur,
-  handleSubmit,
-  getTokenHolderAgreementTags,
-  getReservationAgreementTags,
-  reservationAgreementHash,
-  tokenHolderAgreementHash,
-  neumarkBalance,
-  isAddressSet,
-}) => {
-  const showEther = parseFloat(lockedAmountEth) > 0;
-  const showEuro = parseFloat(lockedAmountEur) > 0;
+export class CommitUnknownUserAftermath extends React.Component<
+  InjectedFormProps<ICommitFundsUnknownUserAftermathForm> & ICommitFundsUnknownUserAftermathProps
+> {
+  public componentDidMount() {
+    if (this.props.initialValues && this.props.initialValues.address) {
+      (this.props.handleSubmit as any)();
+    }
+  }
 
-  return (
-    <div className={styles.aftermath}>
-      <div>
-        <div className={styles.header}>
-          This is a sneak peak of your committed funds in the Neufund ICBM.
-        </div>
-      </div>
+  public render() {
+    const {
+      lockedAmountEth,
+      unlockDateEth,
+      neumarkBalanceEth,
+      lockedAmountEur,
+      unlockDateEur,
+      neumarkBalanceEur,
+      handleSubmit,
+      getTokenHolderAgreementTags,
+      getReservationAgreementTags,
+      reservationAgreementHash,
+      tokenHolderAgreementHash,
+      neumarkBalance,
+      isAddressSet,
+    } = this.props;
 
-      <div className={styles.infoBox}>
-        <h4>Your wallet address:</h4>
-        <div className={styles.value}>
-          <form
-            onChange={() => {
-              // tslint:disable-next-line:jsx-no-lambda
-              // say thank you to redux-form. Is there a better way to achieve auto submitting? what if async validation is required?
-              // using global onChange didnt work (and still there is no way to check validation status)
-              setTimeout(handleSubmit);
-            }}
-          >
-            <Field
-              name="address"
-              component={TextField}
-              validate={ethereumAddressValidator}
-              props={
-                {
-                  inputStyle: estTextFieldStyles.inputStyle,
-                  style: estTextFieldStyles.style,
-                } as any
-              }
-            />
-          </form>
-        </div>
-      </div>
+    const showEther = parseFloat(lockedAmountEth) > 0;
+    const showEuro = parseFloat(lockedAmountEur) > 0;
 
-      {isAddressSet &&
+    return (
+      <div className={styles.aftermath}>
         <div>
-          <div className={styles.infoBox}>
-            <h4>Your NEU balance: </h4>
-            <div className={styles.value}>
-              <MoneyComponent value={neumarkBalance} tokenType={TokenType.NEU} />
-            </div>
+          <div className={styles.header}>
+            This is a sneak peak of your committed funds in the Neufund ICBM.
           </div>
+        </div>
 
-          <div className={styles.infoBox}>
-            <div className={styles.value}>
-              <DownloadDocumentLink
-                key="neumark_token_holder_agreement"
-                documentHash={tokenHolderAgreementHash}
-                getTags={getTokenHolderAgreementTags}
-                outputFilename="neumark_token_holder_agreement"
-              >
-                Token Holder Agreement
-              </DownloadDocumentLink>
-            </div>
+        <div className={styles.infoBox}>
+          <h4>Your wallet address:</h4>
+          <div className={styles.value}>
+            <form
+              onSubmit={e => {
+                // tslint:disable-next-line:jsx-no-lambda
+                e.preventDefault();
+              }}
+              onChange={() => {
+                // tslint:disable-next-line:jsx-no-lambda
+                // say thank you to redux-form. Is there a better way to achieve auto submitting? what if async validation is required?
+                // using global onChange didnt work (and still there is no way to check validation status)
+                setTimeout(handleSubmit);
+              }}
+            >
+              <Field
+                name="address"
+                component={TextField}
+                validate={ethereumAddressValidator}
+                props={
+                  {
+                    hintText: "Enter the address",
+                    inputStyle: estTextFieldStyles.inputStyle,
+                    style: estTextFieldStyles.style,
+                  } as any
+                }
+              />
+            </form>
           </div>
+        </div>
 
-          {showEther &&
-            <CommitmentInfo
-              tokenType={TokenType.ETHER}
-              lockedAmount={lockedAmountEth}
-              neumarkBalance={neumarkBalanceEth}
-              unlockDate={unlockDateEth}
-              reservationAgreementHash={reservationAgreementHash}
-              getReservationAgreementTags={getReservationAgreementTags}
-            />}
+        {isAddressSet &&
+          <div>
+            <div className={styles.infoBox}>
+              <h4>Your NEU balance: </h4>
+              <div className={styles.value}>
+                <MoneyComponent value={neumarkBalance} tokenType={TokenType.NEU} />
+              </div>
+            </div>
 
-          {showEuro &&
-            <CommitmentInfo
-              tokenType={TokenType.EURO}
-              lockedAmount={lockedAmountEur}
-              neumarkBalance={neumarkBalanceEur}
-              unlockDate={unlockDateEur}
-              reservationAgreementHash={reservationAgreementHash}
-              getReservationAgreementTags={getReservationAgreementTags}
-            />}
-        </div>}
-    </div>
-  );
-};
+            {neumarkBalance !== "0" &&
+              <div className={styles.infoBox}>
+                <div className={styles.value}>
+                  <DownloadDocumentLink
+                    key="neumark_token_holder_agreement"
+                    documentHash={tokenHolderAgreementHash}
+                    getTags={getTokenHolderAgreementTags}
+                    outputFilename="neumark_token_holder_agreement"
+                  >
+                    Token Holder Agreement
+                  </DownloadDocumentLink>
+                </div>
+              </div>}
+
+            {showEther &&
+              <CommitmentInfo
+                tokenType={TokenType.ETHER}
+                lockedAmount={lockedAmountEth}
+                neumarkBalance={neumarkBalanceEth}
+                unlockDate={unlockDateEth}
+                reservationAgreementHash={reservationAgreementHash}
+                getReservationAgreementTags={getReservationAgreementTags}
+              />}
+
+            {showEuro &&
+              <CommitmentInfo
+                tokenType={TokenType.EURO}
+                lockedAmount={lockedAmountEur}
+                neumarkBalance={neumarkBalanceEur}
+                unlockDate={unlockDateEur}
+                reservationAgreementHash={reservationAgreementHash}
+                getReservationAgreementTags={getReservationAgreementTags}
+              />}
+          </div>}
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state: IAppState) {
   return {
@@ -179,10 +197,16 @@ const onSubmit: FormSubmitHandler<
   ICommitFundsUnknownUserAftermathForm,
   ICommitFundsUnknownUserAftermathProps
 > = (values, _dispatcher, props) => {
-  props.loadAftermathDetails(values.address);
+  const address = values.address;
+  props.router.push({
+    pathname: "/commit/status",
+    query: { address },
+  });
+  props.loadAftermathDetails(address);
 };
 
 export default compose(
+  withRouter,
   connect(mapStateToProps, mapDispatchToProps),
   reduxForm<ICommitFundsUnknownUserAftermathForm, ICommitFundsUnknownUserAftermathProps>({
     onSubmit,

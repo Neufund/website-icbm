78f2b417e01e4eb28ca41bcc7b091b9d4658ce64
Tomek Banach <tomek@banciur.org>
2017-11-16 14:43:25 +0100

    Add try / catch around tracking calls, remove GA debug mode

--- app/utils/tracking.ts
+++ app/utils/tracking.ts
@@ -6,14 +6,23 @@ import { Web3Type } from "../actions/constants";
 export const trackCommitEvent = (amount: string, web3Type: Web3Type): void => {
   // ga("send", "event", "commit", "web3Type", web3Type.toString());
 
-  // tslint:disable-next-line no-console - it's just placeholder till we get correct code from contractor
-  console.log(
-    `here we are sending commit tracking events to GA, fb, and twitter with ${amount} and ${web3Type}`
-  );
+  try {
+    // tslint:disable-next-line no-console - it's just placeholder till we get correct code from contractor
+    console.log(
+      `here we are sending commit tracking events to GA, fb, and twitter with ${amount} and ${web3Type}`
+    );
+  } catch (e) {
+    // tslint:disable-next-line no-console
+    console.log("error in tracking commit", e);
+  }
 };
 
-// TODO: check if this can be async there are 3 requests that should end before we can proceed
 export const trackMEWEvent = (): void => {
-  // tslint:disable-next-line no-console - it's just placeholder till we get correct code from contractor
-  console.log(`here we are sending MEW tracking events to GA, fb, and twitter`);
+  try {
+    // tslint:disable-next-line no-console - it's just placeholder till we get correct code from contractor
+    console.log(`here we are sending MEW tracking events to GA, fb, and twitter`);
+  } catch (e) {
+    // tslint:disable-next-line no-console
+    console.log("error in tracking MyEtherWallet link click", e);
+  }
 };
